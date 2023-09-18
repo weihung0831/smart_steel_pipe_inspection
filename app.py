e09ed55dace1765f503710256a80d7149b1b6a44
Tim <drum831@gmail.com>
2023-09-19 00:13:15 +0800

    feat:新增前端樣式

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import eel
 
+
 @eel.expose  # 用decorator的方式，將JS要呼叫的PY function暴露給eel, 讓eel當作一個library  去給JS使用
 def say_something(word):
     # 返回 字串加上被呼叫的次數
-    return (
-        f"{word}"
-    )
+    return f"{word}"
 
 
 eel.init("web")  # 設定web的資料夾路徑
-eel.start("main.html")  # 設定要開啟的html檔案路徑
+eel.start("index.html")  # 設定要開啟的html檔案路徑
