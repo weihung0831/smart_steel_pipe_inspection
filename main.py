30fb51a5868330b3d4ba87620d697dde06a2c240
Tim <drum831@gmail.com>
2023-09-27 14:34:12 +0800

    fix:修正控制端語法

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import time
 import sys
 
-sys.path.append("../backend/")
-from backend import ModbusCom, LoggerSetup
+sys.path.append("./backend/")
+from backend import ModbusCom
 
-ip = "192.168.3.5"
-modbus = ModbusCom(ip)
+IP_ADDRESS = "192.168.3.5"
+UP_STATE = 1
+DOWN_STATE = 0
+PLC_INPUT = [i for i in range(0, 16)]
 
-start_time = None
 
-red_light = 0
-orange_light = 1
-green_light = 2
+def handle_stopper_cyl(modbus, plc_input, state):
+    return modbus.set_modbus_output_state(
+        state, modbus.read_modbus_input_state(plc_input)
+    )
 
-try:
-    while True:
-        if modbus.read_modbus_input_state(8):
-            modbus.set_modbus_output_state(green_light, True)
-        else:
-            modbus.set_modbus_output_state(green_light, False)
 
-        if modbus.read_modbus_input_state(9):
-            if start_time == None:
-                start_time = time.time()
-            elif time.time() - start_time >= 0.5:
-                modbus.set_modbus_output_state(orange_light, True)
-        else:
-            start_time = None
-            modbus.set_modbus_output_state(orange_light, False)
+def main():
+    modbus = ModbusCom(IP_ADDRESS)
 
-except KeyboardInterrupt:
-    modbus.clear_output()
-    modbus.disconnect()
-    print("Disconnected")
+    try:
+        while True:
+            handle_stopper_cyl(modbus, PLC_INPUT[8], UP_STATE)
+            handle_stopper_cyl(modbus, PLC_INPUT[9], DOWN_STATE)
+
+    except KeyboardInterrupt:
+        modbus.clear_output()
+        modbus.disconnect()
+        print("Disconnected")
+
+
+if __name__ == "__main__":
+    main()
